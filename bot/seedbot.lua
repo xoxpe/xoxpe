945b2878b6021eca94ecf57daaf2e7529361cce1
xoxpe <mahdizareie83@yahoo.com>
2016-10-19 16:42:42 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {160553465},--Sudo users
+    sudo_users = {160553465},103190020--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
